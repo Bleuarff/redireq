20709eed69148062b6319cda6594abb4b1d5029c
Bleuarff <agoutagny@gmail.com>
2019-07-26 19:11:25 +0200

    Fix manifest & set basic structure
    catch & redirect selected hosts ok.

--- switcherhost.js
+++ switcherhost.js
@@ -1,10 +1,31 @@
+'use strict'
+
+// hosts redirections
+const mapping = {
+  '//news.ycombinator.com': '//www.discoverdev.io'
+}
+const sources = Object.keys(mapping)
+
 function logURL(details){
-  console.log('test: before request')
+  for (let i = 0; i < sources.length; i++){
+    const src = sources[i]
+    if (details.url.indexOf(src) > -1){
+      const newUrl = details.url.replace(src, mapping[src])
+      console.log(`${Date.now()} ${details.url} -> ${newUrl}`)
+      return {
+        redirectUrl: newUrl
+      }
+    }
+  }
 }
 
-browser.webRequest.onBeforeRequest.addListener(
-  logURL,
-  {urls: ['https*://*']}
-)
+function start(){
+  console.log(Date.now() + ' Switcherhost start...')
+  browser.webRequest.onBeforeRequest.addListener(
+    logURL,
+    {urls: ['<all_urls>']},
+    ["blocking"]
+  )
+}
 
-console.log('Switcherhost start...')
+start()
