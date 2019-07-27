10e9463c79997ef43a49d92af1e452643a0f591b
Bleuarff <agoutagny@gmail.com>
2019-07-28 00:42:56 +0200

    Add editor popup basics
    adds an empty row, saves it in config.

---
+++ popup/editor.js
@@ -0,0 +1,66 @@
+'use strict'
+
+/* UI script */
+
+// TODO:
+// - start/stop background script from
+// - bgscript retrieve configs from locale storage on init
+// - show existing confs & handle checkbox
+// - delete conf
+// - style
+
+// start
+console.log('open editor')
+addRow()
+
+const configs = retrieveConfs()
+
+// add new config (handler)
+function addConfig(e){
+  let src = e.currentTarget.parentElement.getElementsByClassName('src')[0].value.trim(),
+      dest = e.currentTarget.parentElement.getElementsByClassName('dest')[0].value.trim()
+
+    // TODO: validate values (via URL?)
+   if (src && dest){
+     configs.push({
+       src: src, dest: dest
+     })
+     // TODO: else: bold border on offender
+     // TODO: update local storage - need permission in manifest?
+
+     // Update background script
+     console.log(configs)
+   }
+
+}
+
+// TODO: get from locale storage
+function retrieveConfs(){
+  return  []
+}
+
+// inserts row in ui, bind controls
+function addRow(data){
+  const checkOrButton = function(hasData){
+    if (hasData)
+      return '<input type="checkbox"><label>Enabled</label>'
+    else
+      return '<button id="add-btn">Add</button>'
+  }
+
+  const hasData = !!data
+  data = data || {}
+  const nd = document.createElement('div')
+  nd.classList.add('row')
+  const tmpl = `
+    <input type="text" class="src" placeholder="source host"></input>
+    →
+    <input type="text" class="dest" placeholder="destination host"></input>
+    ${checkOrButton(hasData)}
+  `
+  nd.innerHTML = tmpl
+
+  const ctnr = document.getElementById('row-ctnr')
+  ctnr.appendChild(nd)
+  document.getElementById('add-btn').addEventListener('click', addConfig)
+}
