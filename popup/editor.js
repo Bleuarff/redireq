ca45b342586e80feea814e392d19cd16b344ad83
Bleuarff <agoutagny@gmail.com>
2019-07-28 21:23:03 +0200

    Configs retrieved from storage + misc
    clear input fields after save
    simplify editor js

--- popup/editor.js
+++ popup/editor.js
@@ -10,60 +10,63 @@
 
 // start
 console.log('open editor')
-addRow()
+const STORAGE_KEY = 'configs'
+const configs = JSON.parse(window.localStorage.getItem(STORAGE_KEY)) || []
 
-const configs = retrieveConfs()
+init()
 
-// add new config (handler)
+function init(){
+  console.debug(`found ${configs.length} configs stored.`)
+  configs.forEach(cfg => {
+    addRow(cfg)
+  })
+  // bind add config button
+  document.getElementById('add-btn').addEventListener('click', addConfig)
+}
+
+// add button handler: create new config
 async function addConfig(e){
-  let src = e.currentTarget.parentElement.getElementsByClassName('src')[0].value.trim(),
-      dest = e.currentTarget.parentElement.getElementsByClassName('dest')[0].value.trim()
+  const srcNd = e.currentTarget.parentElement.getElementsByClassName('src')[0],
+        src = srcNd.value.trim(),
+        destNd = e.currentTarget.parentElement.getElementsByClassName('dest')[0],
+        dest = destNd.value.trim()
+
 
     // TODO: validate values (via URL?)
    if (src && dest){
-     configs.push({
-       src: src, dest: dest
-     })
-     // TODO: else: bold border on offender
-     // TODO: update local storage
+     const newConf = { src: src, dest: dest, enabled: true }
+     configs.push(newConf)
 
      // Update background script
      try{
        await browser.runtime.sendMessage(configs)
+       // update local storage
+       window.localStorage.setItem(STORAGE_KEY, JSON.stringify(configs))
+       addRow(newConf) // add row to list
+       srcNd.value = '' // empty new row fields
+       destNd.value = ''
      }
      catch(ex){
        console.error(ex)
+       // TODO: show error message
      }
    }
-}
-
-// TODO: get from locale storage
-function retrieveConfs(){
-  return  []
+   // TODO: else: bold red border on offender
 }
 
 // inserts row in ui, bind controls
-function addRow(data){
-  const checkOrButton = function(hasData){
-    if (hasData)
-      return '<input type="checkbox"><label>Enabled</label>'
-    else
-      return '<button id="add-btn">Add</button>'
-  }
-
-  const hasData = !!data
-  data = data || {}
+function addRow(data = { src: '', dest: '', enabled: true}){
   const nd = document.createElement('div')
   nd.classList.add('row')
   const tmpl = `
-    <input type="text" class="src" placeholder="source host"></input>
+    <input type="text" class="src" placeholder="source host" value="${data.src}"></input>
     →
-    <input type="text" class="dest" placeholder="destination host"></input>
-    ${checkOrButton(hasData)}
+    <input type="text" class="dest" placeholder="destination host" value="${data.dest}"></input>
+    <input type="checkbox" ${data.enabled ? 'checked' : ''}><label>Enabled</label>
   `
   nd.innerHTML = tmpl
 
   const ctnr = document.getElementById('row-ctnr')
   ctnr.appendChild(nd)
-  document.getElementById('add-btn').addEventListener('click', addConfig)
+
 }
