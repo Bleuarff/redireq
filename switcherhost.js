19c1bd76d761643abfbc26a74a08947609d2ab0f
Bleuarff <agoutagny@gmail.com>
2019-07-28 14:35:31 +0200

    Refactor background script: methods put in class
    clean globale space

--- switcherhost.js
+++ switcherhost.js
@@ -1,56 +1,62 @@
 'use strict'
 
-// hosts redirections
-const mapping = {
-  '//news.ycombinator.com': '//www.discoverdev.io'
-}
+// handles binding onBeforeRequest & redirections
+class Switcherhost{
+  constructor(){
+    // hosts redirections
+    this.mapping = {
+      '//news.ycombinator.com': '//www.discoverdev.io'
+    }
+    this.sources = Object.keys(this.mapping)
+  }
 
-const sources = Object.keys(mapping)
 
-// handler when a request matches the url filter.
-// looks for host to replace, build new url and return
-function redirect(details){
-  let newUrl
+  // handler when a request matches the url filter.
+  // looks for host to replace, build new url and return
+  redirect(details){
+    let newUrl
 
-  for (let i = 0; i < sources.length; i++){
-    const src = sources[i]
-    if (details.url.indexOf(src) > -1){
-      newUrl = details.url.replace(src, mapping[src])
-      break
+    for (let i = 0; i < this.sources.length; i++){
+      const src = this.sources[i]
+      if (details.url.indexOf(src) > -1){
+        newUrl = details.url.replace(src, this.mapping[src])
+        break
+      }
+    }
+
+    if (newUrl){
+      console.log(`${Date.now()} ${details.url} -> ${newUrl}`)
+      return {
+        redirectUrl: newUrl
+      }
     }
   }
 
-  if (newUrl){
-    console.log(`${Date.now()} ${details.url} -> ${newUrl}`)
-    return {
-      redirectUrl: newUrl
-    }
+  start(){
+    console.log(Date.now() + ' Switcherhost start...')
+
+    // build filter list for source hosts, so as to not watch for every request
+    const urlFilter = this.sources.map(x => '*:' + x + '/*')
+
+    browser.webRequest.onBeforeRequest.addListener(
+      (details) => {return this.redirect(details)},
+      {urls: urlFilter},
+      ["blocking"]
+    )
   }
+
+  // stop(){
+  //   console.log(Date.now() + ' stop.')
+  //
+  //   const bound = browser.webRequest.onBeforeRequest.hasListener(start)
+  //   console.log('listener bound: ' + bound)
+  //
+  //   browser.webRequest.onBeforeRequest.removeListener(start)
+  //
+  //   bound = browser.webRequest.onBeforeRequest.hasListener(start)
+  //   console.log('listener bound: ' + bound)
+  // }
 }
 
-function start(){
-  console.log(Date.now() + ' Switcherhost start...')
-
-  // build filter list for source hosts, so as to not watchvor every request
-  const urlFilter = sources.map(x => '*:' + x + '/*')
-
-  browser.webRequest.onBeforeRequest.addListener(
-    redirect,
-    {urls: urlFilter},
-    ["blocking"]
-  )
-}
-
-// function stop(){
-//   console.log(Date.now() + ' stop.')
-//
-//   const bound = browser.webRequest.onBeforeRequest.hasListener(start)
-//   console.log('listener bound: ' + bound)
-//
-//   browser.webRequest.onBeforeRequest.removeListener(start)
-//
-//   bound = browser.webRequest.onBeforeRequest.hasListener(start)
-//   console.log('listener bound: ' + bound)
-// }
-
-start()
+const swh = new Switcherhost()
+swh.start()
