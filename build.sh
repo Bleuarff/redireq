28982620e0df336819472b5df9bca1c776f22bba
bleuarff <bleuarff@gmail.com>
2019-09-10 22:20:06 +0200

    rename icons folder to images

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 # Builds zip file with all files needed to publish the extension
 
 filename="redireq_$(date --utc +%Y%m%d_%H%M)"
-zip -q $filename icons/*.png popup/* background.js manifest.json
+zip -q $filename images/logo-*.png popup/* background.js manifest.json
 echo "$filename.zip OK"
